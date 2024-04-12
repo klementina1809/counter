5aa9a597814a5237698a32b6f0834865c78ff39c
klementina1809 <klementina.ua@gmail.com>
2024-04-12 20:14:25 +0200

    settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,150 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useLocalStorage from "use-local-storage";
+import { Container, Row } from "react-grid-system";
+
 import Input from "./components/Input";
 import User from "./components/User";
 
 import "./App.css";
 
 function App() {
-	const [bet, setBet] = useState(0);
+	const [currentUser, setCurrentUser] = useState(0);
 	const [value, setValue] = useState("");
-
 	const [users, setUsers] = useLocalStorage("users", [
-		{ name: "Davide", money: 0 },
-		{ name: "Alessandro", money: 0 },
-		{ name: "Federico", money: 0 },
-		{ name: "Matteo", money: 0 },
+		{ id: 1, name: "Davide", money: 0 },
+		{ id: 2, name: "Alessandro", money: 0 },
+		{ id: 3, name: "Federico", money: 0 },
+		{ id: 4, name: "Matteo", money: 0 },
 	]);
+
+	useEffect(() => {
+		console.log("currentUser", currentUser);
+	}, [currentUser]);
 
 	const handleChange = (e) => {
 		const newValue = e.target.value;
 		setValue(newValue);
 	};
-  const handleDelete = (name) => {
-    const newUsers = users.filter((user) => user.name !== name);
-    setUsers(newUsers);
-};
 
-	const handleAdd = () => {
-    if (value.trim() === "") return;
+	const handleDelete = (id) => {
+		const newUsers = users.filter((user) => user.id !== id);
+		setUsers(newUsers);
+	};
+	const handleClear = () => {
+		const updatedUsers = users.map((user) => ({ ...user, money: 0 }));
+		setUsers(updatedUsers);
+		console.log("cleared");
+	};
+
+	const generateId = () => {
+		const id = users[users.length - 1].id + 1;
+		console.log(id);
+		return id;
+	};
+
+	const handleAddUser = (e) => {
+		e.preventDefault();
+		if (value.trim() === "") return;
 		setValue("");
-		const newUser = { name: value, money: 0 };
-		setUsers((prev) => [...prev, newUser]);
+		const newUser = { id: generateId(), name: value, money: 0 };
+		setUsers([...users, newUser]);
 		console.log("Submitted");
 	};
 
+	const handleAddMoney = (fish) => {
+		const userIndex = users.findIndex((user) => user.id === currentUser);
+		const updatedMoney = users[userIndex].money + fish;
+		const updatedUser = { ...users[userIndex], money: updatedMoney };
+		const updatedUsers = [...users];
+		updatedUsers[userIndex] = updatedUser;
+		setUsers(updatedUsers);
+	};
+
+	const selectUser = (id) => {
+		setCurrentUser(id);
+	};
+
 	return (
-		<>
-			<form onSubmit={(e) => e.preventDefault(handleAdd ())}>
-				<div className="form">
-					<Input
-						name={"user"}
-						value={value}
-						onChange={(e) => handleChange(e)}
-					/>
-					<button
-						type="submit"
-						className="p-2 border-2 font-semibold bg-red-400 text-white border-red-400 hover:bg-red-500 focus:outline-none"
-					>
-						Add
-					</button>
-				</div>
-			</form>
-			<div className="users">
+		<Container>
+			<Row>
+				<form onSubmit={handleAddUser}>
+					<div className="form">
+						<Input
+							name={"user"}
+							value={value}
+							onChange={(e) => handleChange(e)}
+						/>
+						<button
+							type="submit"
+							className="p-2 border-2 rounded-md font-semibold bg-red-400 text-white border-red-400 hover:bg-red-500 "
+						>
+							Add
+						</button>
+					</div>
+				</form>
+			</Row>
+			<div className="users mb-4 mt-4 ">
 				{users.map((user) => (
 					<User
-						key={user.name}
-						name={user.name}
-						money={user.money}
+						key={user.id}
+						user={user}
 						onDelete={handleDelete}
+						onClick={() => selectUser(user.id)}
+						currentUser={currentUser}
 					/>
 				))}
 			</div>
-			<div className="chips">
+			<div className="chips my-8">
 				<div className="chip" data-value="0.5">
 					<img
 						src="./img/10.png"
 						alt=""
-						onClick={() => setBet(0.5)}
+						onClick={() => handleAddMoney(0.5)}
 					/>
 				</div>
 				<div className="chip" data-value="1">
-					<img src="./img/20.png" alt="" onClick={() => setBet(1)} />
+					<img
+						src="./img/20.png"
+						alt=""
+						onClick={() => handleAddMoney(1)}
+					/>
 				</div>
 				<div className="chip" data-value="5">
-					<img src="./img/50.png" alt="" onClick={() => setBet(5)} />
+					<img
+						src="./img/50.png"
+						alt=""
+						onClick={() => handleAddMoney(5)}
+					/>
 				</div>
 				<div className="chip" data-value="-0.5">
 					<img
 						src="./img/10.png"
 						alt=""
-						onClick={() => setBet(0.5)}
+						onClick={() => handleAddMoney(-0.5)}
 					/>
 				</div>
 				<div className="chip" data-value="-1">
-					<img src="./img/20.png" alt="" onClick={() => setBet(1)} />
+					<img
+						src="./img/20.png"
+						alt=""
+						onClick={() => handleAddMoney(-1)}
+					/>
 				</div>
 				<div className="chip" data-value="-5">
-					<img src="./img/50.png" alt="" onClick={() => setBet(5)} />
+					<img
+						src="./img/50.png"
+						alt=""
+						onClick={() => handleAddMoney(-5)}
+					/>
 				</div>
 			</div>
-		</>
+			<button
+				onClick={handleClear}
+				className="p-2 border-2 rounded-md font-semibold bg-gray-400 text-white border-gray-400 hover:bg-gray-500"
+			>
+				Clear
+			</button>
+		</Container>
 	);
 }
 
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Row, Col } from "react-grid-system";
 
-function User({name, money, onDelete}) {
-  return (
-    <div className='user'>
-      <span>{name}</span>
-      <span>{money}</span>
-      <button onClick={onDelete(name)}>x</button>
-    </div>
-  )
+function User({ user, currentUser, onDelete, onClick }) {
+	return (
+		<Row className="user">
+			<Col sm={2}/>
+			<Col sm={3}>
+				<span
+					onClick={() => onClick(user)}
+					className={`flex text-left font-semibold ${
+						user.id === currentUser ? "text-red-500" : ""
+					}`}
+				>
+					{user.name}
+				</span>
+			</Col>
+			<Col sm={2}>
+				<span>{user.money}</span>
+        
+			</Col>
+			<Col sm={2}>
+				<button
+					className="px-2 my-2 border-2 rounded-md bg-red-400 text-white border-red-400 hover:bg-red-500 focus:outline-none"
+					onClick={() => onDelete(user.id)}
+				>
+					x
+				</button>
+			</Col>
+			<Col sm={2} />
+		</Row>
+	);
 }
 
-export default User
+export default User;
